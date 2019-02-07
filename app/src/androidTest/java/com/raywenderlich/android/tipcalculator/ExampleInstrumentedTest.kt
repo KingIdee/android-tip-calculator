7b82204194644532294cc9ff7c6c1adda28e0cf8
Idorenyin Obong <idee4ril@gmail.com>
2019-02-07 08:08:42 +0100

    final project is ready

--- app/src/androidTest/java/com/raywenderlich/android/tipcalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/tipcalculator/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package tipcalculator.raywenderlich.com.tipcalculator
+package com.raywenderlich.android.tipcalculator
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -15,10 +15,10 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("tipcalculator.raywenderlich.com.tipcalculator", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getTargetContext()
+    assertEquals("tipcalculator.raywenderlich.com.tipcalculator", appContext.packageName)
+  }
 }
